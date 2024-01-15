85a42f6e34c5f619610b2183bea7a78c25fb7f92
Samurai <127001sudais01@gmail.com>
2024-01-15 12:40:54 +0800

    config for cypress test

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,18 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    // Assuming your e2e tests are in cypress/e2e
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+  },
+  component: {
+    // Assuming your component tests are in cypress/component
+    specPattern: "cypress/component/**/*.cy.{js,jsx,ts,tsx}",
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
     },
   },
 });
---
+++ cypress/component/GameboardUI.cy.jsx
@@ -0,0 +1,5 @@
+describe('GameboardUI.cy.jsx', () => {
+  it('playground', () => {
+    // cy.mount()
+  })
+})
